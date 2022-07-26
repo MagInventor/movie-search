8b1e7b18f4cc39f71a051d1af81f4a22fa312695
dreamtogostar <dreamerstargazer@yandex.ru>
2022-07-26 19:16:30 +0300

    feat: add form activation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './index.css';
-import wrapper from './js/components/Wrapper.component.js';
+import favicon from './js/components/Favicon';
+import wrapper from './js/components/Wrapper.component';
+import Form from './js/search/Form';
+
 
 wrapper.render();
+const form = new Form();
+form.activationForm();
+form.onSumbit();
+favicon.addFavicon();
